5f5390f8ef31bdb5e299e706d5ec2680400cb47b
sayumiiii <sayumihasanjali@icloud.com>
2023-03-14 17:37:41 -0400

    Revert "deploy"
    This reverts commit d8f0aedd83d74b65f32093841eeb71d9defcce4b.

--- script.js
+++ script.js
@@ -1,25 +1,3 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyBeyGVWMxuuqaPouXPfNxyjB10_EXY-2-Q",
-  authDomain: "bookclub-d5227.firebaseapp.com",
-  projectId: "bookclub-d5227",
-  storageBucket: "bookclub-d5227.appspot.com",
-  messagingSenderId: "1005898698080",
-  appId: "1:1005898698080:web:534f3546d8260306326205",
-  measurementId: "G-NP39ZL9SXZ"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-
 searchForm = document.querySelector('.search-form');
 
 document.querySelector('#search-btn').onclick = () =>{
